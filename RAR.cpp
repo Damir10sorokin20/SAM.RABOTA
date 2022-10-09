635b341945b697dceb33e1c866c9eca52656366e
Damir10sorokin20 <114170327+Damir10sorokin20@users.noreply.github.com>
2022-10-09 18:41:44 +0300

    Add files via upload

---
+++ RAR.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Rus");
+	//boolean1
+	//int hp;
+	//cin >> hp;
+	//bool r;
+	//r = hp > 0;
+	//cout << boolalpha << r;
+	//if2
+	//int c;
+	//cin >> c;
+	//if (c > 20) {
+		//cout << "on";
+	//}
+	//else {
+		//cout << "off";
+	//}
+	//if3
+	//int c, t, vl;
+	//cin >> t >> vl>>c;
+	//if (c > t && vl <= 80) {
+		//cout << "on";
+	//}
+	//else {
+		//cout << "off";
+	//}
+	//if4
+	//float x, y;
+	//cin >> x;
+	//if (x > 0) {
+		//y = sqrt(x - 2);
+		//cout << y;
+	//}
+	//else if (x == 0) {
+		//y = 0;
+		//cout << y;
+	//}
+	//else if (x < 0) {
+		//y = abs(x);
+		//cout << y;
+	//}
+	//switch5
+	//int a;
+	//cin >> a;
+	//switch (a) {
+	//case 1:cout << "большой палец"; break;
+	//case 2:cout << "безымянный палец"; break;
+	//case 3:cout << "средний палец"; break;
+	//case 4:cout << "указательный палец"; break;
+	//case 5:cout << "мизинец"; break;
+	//default:cout << "фатальная ошибка"; break;
+	//}
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+//}
